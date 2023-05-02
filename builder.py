d16809e67b5a34327cff3b28a4562268e49e78a1
federico berra <fede.berra@hotmail.it>
2023-05-02 14:03:31 +0200

    Added main structure for the text layout

--- builder.py
+++ builder.py
@@ -5,7 +5,32 @@ from PIL import ImageFont
 # Open an Image
 img = Image.open('template/blank-template.png')
 I1 = ImageDraw.Draw(img)
-myFont = ImageFont.truetype('Roboto-Regular.ttf', 65)
-I1.text((28, 40), "nice Car", font=myFont, fill=(255, 0, 0))
+
+# parameters
+height = 1037
+width = 754
+
+font_size = 30
+interline = 5
+padding = 30
+color = (255, 0, 0)
+text = ImageFont.truetype('Roboto-Regular.ttf', 30)
+new_line = font_size + interline
+ability_row = 90
+ability_line = 470
+action_line = 700
+
+# drawings
+I1.text((ability_row, padding), "Name", font=text, fill=color)
+I1.text((width - padding - font_size * 2, padding), "99", font=text, fill=color)
+I1.text((ability_row, padding + new_line), "Type", font=text, fill=color)
+I1.text((ability_row, padding + 2 * new_line), "Grade", font=text, fill=color)
+I1.text((ability_row, ability_line), "Ability", font=text, fill=color)
+I1.text((ability_row, ability_line + new_line), "aaaaaaaaa", font=text, fill=color)
+
+I1.text((ability_row, action_line), "Action", font=text, fill=color)
+I1.text((ability_row, action_line + new_line), "aaaaaaaaa", font=text, fill=color)
+I1.text((width - ability_row - font_size * 2, action_line), "99", font=text, fill=color)
+
 img.show()
 # img.save("cards/test.png")
