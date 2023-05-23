7392e3e6660de38b6ff44b4d48b192aafa0d2565
federico berra <fede.berra@hotmail.it>
2023-05-23 18:54:44 +0200

    Improved texts and balance the game

--- builder.py
+++ builder.py
@@ -13,13 +13,13 @@ width = 754
 
 font_size = 30
 interline = 5
-padding = 30
+padding = font_size
 color = (255, 0, 0)
-text = ImageFont.truetype('Roboto-Regular.ttf', 30)
+text = ImageFont.truetype('Roboto-Regular.ttf', font_size)
 new_line = font_size + interline
-effect_row = 90
+effect_x_value = 90
 effect_line = 470
-action_line = 700
+attack_line = 700
 
 
 def draw_multiple_line_text(img, xy, text, font, fill):
@@ -34,30 +34,30 @@ def draw_multiple_line_text(img, xy, text, font, fill):
         y_text += line_height
 
 
-def print_card(img, name, type, project_points=None, grade=None, effect_description=None, action_cost=None, action_description=None, action_steal_points=None, text_color=color):
+def print_card(img, name, type, project_points=None, grade=None, effect_description=None, attack_cost=None, attack_description=None, attack_dmg=None, text_color=color):
     draw = ImageDraw.Draw(img)
-    draw.text((effect_row, padding), name, font=text, fill=text_color)
+    draw.text((effect_x_value, padding), name, font=text, fill=text_color)
 
     if not pd.isna(project_points):
         draw.text((width - padding - font_size * 2, padding), str(int(project_points)), font=text, fill=text_color)
 
-    draw.text((effect_row, padding + new_line), type, font=text, fill=text_color)
+    draw.text((effect_x_value, padding + new_line), type, font=text, fill=text_color)
 
     if not pd.isna(grade):
-        draw.text((effect_row, padding + 2 * new_line), grade, font=text, fill=text_color)
+        draw.text((effect_x_value, padding + 2 * new_line), grade, font=text, fill=text_color)
 
     if not pd.isna(effect_description):
-        draw_multiple_line_text(img, (effect_row, effect_line), "Effect", font=text, fill=text_color)
-        draw_multiple_line_text(img, (effect_row, effect_line + new_line), effect_description, font=text, fill=text_color)
-    if not pd.isna(action_description):
-        draw_multiple_line_text(img, (effect_row, action_line + new_line), action_description, font=text, fill=text_color)
-    if not pd.isna(action_cost):
-        draw.text((effect_row, action_line), "Action", font=text, fill=text_color)
-        draw.text((effect_row + 100, action_line), action_cost, font=text, fill=text_color)
-        if pd.api.types.is_number(action_steal_points):
-            draw.text((width - effect_row - font_size * 2, action_line), str(int(action_steal_points)), font=text, fill=text_color)
+        draw_multiple_line_text(img, (effect_x_value, effect_line), "Effect", font=text, fill=text_color)
+        draw_multiple_line_text(img, (effect_x_value, effect_line + new_line), effect_description, font=text, fill=text_color)
+    if not pd.isna(attack_description):
+        draw_multiple_line_text(img, (effect_x_value, attack_line + new_line), attack_description, font=text, fill=text_color)
+    if not pd.isna(attack_cost):
+        draw.text((effect_x_value, attack_line), "Attack", font=text, fill=text_color)
+        draw.text((effect_x_value + 100, attack_line), attack_cost, font=text, fill=text_color)
+        if pd.api.types.is_number(attack_dmg):
+            draw.text((width - effect_x_value - font_size * 2, attack_line), str(int(attack_dmg)), font=text, fill=text_color)
         else:
-            draw.text((width - effect_row - font_size * 2, action_line), action_steal_points, font=text, fill=text_color)
+            draw.text((width - effect_x_value - font_size * 2, attack_line), attack_dmg, font=text, fill=text_color)
 
 
 # MAIN
@@ -70,13 +70,13 @@ for index, row in df.iterrows():
     if row["type"] == "coin":
         img = Image.open('template/coin-template.png')
         print_card(img=img, name=row["public name"], type=row["type"], text_color=(0, 0, 0))
-    if row["type"] in ("(F) FPGA", "(P) PAT", "(U) Pure", "(Q) QRNG", "(S) Source", "(W) Weak"):
+    if row["type"] in ("[F] FPGA", "[P] PAT", "[U] Pure", "[Q] QRNG", "[S] Source", "[W] Weak"):
         if row["grade"] == "phd":
             img = Image.open('template/phd-template.png')
         else:
             img = Image.open('template/post-template.png')
         print_card(img=img, name=row["public name"], type=row["type"], effect_description=row["effect"],
-                   project_points=row["project points"], grade=row["grade"], action_cost=row["action cost"], action_description=row["action"], action_steal_points=row["steal points"],
+                   project_points=row["project points"], grade=row["grade"], attack_cost=row["attack cost"], attack_description=row["attack"], attack_dmg=row["dmg"],
                    text_color=(0, 0, 0))
     if row["type"] == "professor":
         img = Image.open('template/prof-template.png')
